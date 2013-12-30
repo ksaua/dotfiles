78dcb488b65d5ac2eaeca4e9e094166454b1df1f
Knut Saua Mathiesen <git@canutes.net>
2013-12-30 21:05:20 +0100

    Leiningen: Added lein profile.clj

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,3 +6,7 @@ ln -s $HOME/.dotfiles/emacs/init.el $HOME/.emacs.d/init.el
 
 # git
 ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
+
+# lein
+mkdir -p $HOME/.lein
+ln -s $HOME/.dotfiles/lein/profiles.clj $HOME/.lein/profiles.clj
